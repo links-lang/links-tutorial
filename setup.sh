ebaeb19a57907f4e040baae5fc9eabe2b4c21f25
James Cheney <jcheney@inf.ed.ac.uk>
2017-09-21 23:14:24 +0100

    * added missing dependency

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt install -y m4 postgresql libpq-dev opam
+sudo apt install -y m4 postgresql libpq-dev pkg-config opam
 opam init -a
 opam switch 4.04.0
 eval `opam config env`
