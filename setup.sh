a639b26bc8be75043e250f7d34a06bf9870b55cd
James Cheney <jcheney@inf.ed.ac.uk>
2018-01-24 15:23:51 +0000

    * 0.7.2

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo apt install -y m4 postgresql libpq-dev pkg-config opam
 opam init -a
 opam switch 4.04.0
 eval `opam config env`
-opam install -y postgresql links
+opam install -y postgresql links links-postgresql
 ln -s /vagrant links-tutorial
 
