6afaa4d82060f1293dfa37effe81d532aeaacd5a
James Cheney <jcheney@inf.ed.ac.uk>
2018-06-12 13:36:07 +0100

    * update to 0.7.3

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install -y m4 postgresql libpq-dev pkg-config opam
 opam init -a
-opam switch 4.04.0
+opam switch 4.06.0
 eval `opam config env`
 opam install -y postgresql links links-postgresql
 ln -s /vagrant links-tutorial
